96a4bcb97b9b38c21dfa58a629e9fc4adb6f8130
sayandcode <sayandcode@gmail.com>
2022-05-10 15:23:38 +0530

    Add homePage render method
    Shows a maximum of two taskGroups

--- src/js/pages/homePage.js
+++ src/js/pages/homePage.js
@@ -1,5 +1,12 @@
+import { Task, TaskList } from "../tasks&Projects/tasks";
+import createTaskGroups from "./taskGroups";
+
 const homePage= function(){
-    return 'placeholder';
+    const allTasks=TaskList.groupByDate(Task.all);
+    const indexOfPast=allTasks.findIndex(element=>element.name==='In the Past');
+    if(indexOfPast!==-1)
+        allTasks.splice(indexOfPast,1);
+    return createTaskGroups.generate(allTasks.splice(0,2));
 };
 
 export default homePage;
